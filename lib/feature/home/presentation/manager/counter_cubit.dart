70043116c142cc0ace37529f32dbb2a3c43e9cc9
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-04-16 13:55:49 +0200

    create counter cubit

---
+++ lib/feature/home/presentation/manager/counter_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:adkar/core/database/cache/cache_helper.dart';
+import 'package:adkar/core/database/cache/cache_keys.dart';
+import 'package:adkar/core/service_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0) {
+    loadValue();
+  }
+
+  void loadValue() {
+    final counterValue =
+        getIt.get<CacheHelper>().getInt(CacheKeys.counterKey) ?? 0;
+    emit(counterValue);
+  }
+
+  void increamentValue() {
+    final newValue = state + 1;
+    getIt.get<CacheHelper>().setInt(CacheKeys.counterKey, newValue);
+    emit(newValue);
+  }
+
+  void resetValue() {
+    emit(0);
+    getIt.get<CacheHelper>().setInt(CacheKeys.counterKey, 0);
+  }
+}
